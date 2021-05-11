a9c5a42f23f329f3703d144912e870fdc339612f
Yang ShaoPing <83759641+HandsomeYang1@users.noreply.github.com>
2021-05-11 10:34:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -355,9 +355,9 @@ def printfile():
 #     st.write(jishu + '挂科率',k)
 
 def get_table_download_link(df,file_name):
-    excel = df.to_excel(index=False,encoding='gbk',excel_writer=file_name+'.xlsx')
-    b64 = base64.b64encode(excel.encode()).decode()  # some strings <-> bytes conversions necessary here
-    return f'<a href="data:file/excel;base64,{b64}" download="{file_name}.xlsx">Download {file_name} file</a>'
+    csv = df.to_csv(index=False,encoding='gbk')
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}" download="{file_name}.csv">Download {file_name} file</a>'
 
 if file is None:
     st.write('目前文件为空，请上传您需要处理的南昌航空大学(xxxx级成绩列表.csv)文件')
